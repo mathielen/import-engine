ac9f269a566ffbebddda408559bcc413d83e2864
Markus Thielen <info@logicx.de>
2015-05-07 11:53:18 +0200

    caching already de-serialized objects

--- src/Mathielen/DataImport/Writer/ObjectWriter/JmsSerializerObjectFactory.php
+++ src/Mathielen/DataImport/Writer/ObjectWriter/JmsSerializerObjectFactory.php
@@ -13,6 +13,11 @@ class JmsSerializerObjectFactory implements ObjectFactoryInterface
      */
     private $serializer;
 
+    /**
+     * @var array
+     */
+    private $cache = [];
+
     public function __construct($classname, SerializerInterface $serializer)
     {
         $this->classname = $classname;
@@ -25,7 +30,17 @@ class JmsSerializerObjectFactory implements ObjectFactoryInterface
         $item = array_change_key_case($item, CASE_LOWER);
         $json = json_encode($item);
 
+        $cacheKey = md5($json);
+        if (isset($this->cache[$cacheKey])) {
+            return $this->cache[$cacheKey];
+        }
+
         $object = $this->serializer->deserialize($json, $this->classname, 'json');
+        $this->cache[$cacheKey] = $object;
+
+        if (count($this->cache) > 100) {
+            array_shift($this->cache); //cut last one
+        }
 
         return $object;
     }
