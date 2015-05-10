082d1386605df06d305076ddffc1026759432c7d
Markus Thielen <info@logicx.de>
2015-05-10 23:18:22 +0200

    exception catch

--- src/Mathielen/DataImport/ValueConverter/GenericDateItemConverter.php
+++ src/Mathielen/DataImport/ValueConverter/GenericDateItemConverter.php
@@ -18,13 +18,17 @@ class GenericDateItemConverter extends DateTimeValueConverter
         }
 
         // dd.mm.yyyy, dd-mm-yyyy
-        if (preg_match('/^([0-9]{1,2})[\.-]([0-9]{1,2})[\.-]([0-9]{2,4})$/', $input, $matches)) {
-            $date = new \DateTime($matches[3].'-'.$matches[2].'-'.$matches[1]);
-        } else {
-            $date = new \DateTime($input);
-        }
+        try {
+            if (preg_match('/^([0-9]{1,2})[\.-]([0-9]{1,2})[\.-]([0-9]{2,4})$/', $input, $matches)) {
+                $date = new \DateTime($matches[3] . '-' . $matches[2] . '-' . $matches[1]);
+            } else {
+                $date = new \DateTime($input);
+            }
 
-        return $this->formatOutput($date);
+            return $this->formatOutput($date);
+        } catch (\Exception $e) {
+            return $input;
+        }
     }
 
     protected function formatOutput(\DateTime $date)
