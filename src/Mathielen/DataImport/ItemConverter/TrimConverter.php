ce0032e9c42e5702d89fb2719f47e26504897fb3
Markus Thielen <info@logicx.de>
2015-08-12 10:20:11 +0200

    +trimconverter

---
+++ src/Mathielen/DataImport/ItemConverter/TrimConverter.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mathielen\DataImport\ItemConverter;
+
+use Ddeboer\DataImport\ItemConverter\ItemConverterInterface;
+
+class TrimConverter implements ItemConverterInterface
+{
+
+    public function convert($input)
+    {
+        return is_array($input)?array_map(function($item) { return trim($item); }, $input):trim($input);
+    }
+
+}
